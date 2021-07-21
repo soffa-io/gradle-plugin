c58d4fa50d603ef0fc34ec4ab2bd9686758996f8
Titus GAHISSY <titus@gahissy.com>
2021-07-22 00:55:25 +0100

    Publish to private maven repo

--- src/main/groovy/io/soffa/tools/gradle/maven/MavenPublishPlugin.groovy
+++ src/main/groovy/io/soffa/tools/gradle/maven/MavenPublishPlugin.groovy
@@ -16,6 +16,24 @@ class MavenPublishPlugin implements Plugin<Project> {
                 artifactId = project.name
             }
         }
+
+
+        String publicationUrl = project.findProperty("publication.url") ?: System.getenv("MVN_PUBLICATION_URL")
+        String publicationUser = project.findProperty("publication.username") ?: System.getenv("MVN_PUBLICATION_USER")
+        String publicationPassword = project.findProperty("publication.password") ?: System.getenv("MVN_PUBLICATION_PASSWORD")
+
+        if (publicationUrl !=null && publicationUser != null && publicationPassword!=null) {
+            println("Configuration maven publication: ${publicationUrl} / ${publicationUser}")
+            project.publishing.repositories {
+                maven {
+                    url = publicationUrl
+                    credentials {
+                        username = publicationUser
+                        password = publicationPassword
+                    }
+                }
+            }
+        }
     }
 
 }
