7615eea2557b2bdc3473a15561532ad586f05ab9
Titus Gahissy <titus@gahissy.com>
2021-11-23 16:57:01 +0000

    Fix spring boot project publication

--- src/main/groovy/io/soffa/tools/gradle/service/SpringBootServicePlugin.groovy
+++ src/main/groovy/io/soffa/tools/gradle/service/SpringBootServicePlugin.groovy
@@ -8,7 +8,12 @@ class SpringBootServicePlugin extends SpringBootDependencyPlugin {
         project.plugins.apply("org.springframework.boot")
         super.apply(project)
         // project.dependencies.add("testImplementation", "org.springframework.boot:spring-boot-starter-test")
-
+        project.jar {
+            enabled = false
+        }
+        project.bootJar {
+            classifier = 'application'
+        }
     }
 
 }
